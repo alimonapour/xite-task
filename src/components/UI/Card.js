66e6c969574ec690dd27dabecd673306576f0567
alimonapour <a.monapoor@gmail.com>
2022-09-25 01:41:11 +0330

    re style som element

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,13 @@
 export const Card = ({ artist, title, imageUrl }) => {
   return (
-    <div className='w-full rounded overflow-hidden shadow-lg m-2'>
+    <div className='rounded overflow-hidden shadow-lg'>
       <img className='w-full h-64 object-center' src={imageUrl} alt={artist} />
       <div className='px-6 py-2'>
-        <div className='font-regular flex flex-col items-center justify-around'>
-          <h1 className='text-xl'>{artist}</h1>
-          <h2 className='text-md '>{title}</h2>
+        <div className='flex flex-col items-center'>
+          <h1 className='w-full text-center text-base tracking-tighter font-semibold'>
+            {artist}
+          </h1>
+          <h2 className='text-sm tracking-tighter mt-1 text-center'>{title}</h2>
         </div>
       </div>
     </div>
