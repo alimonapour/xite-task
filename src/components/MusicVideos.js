6d59346f2363cde2260f600e2b1e51e028d9b737
alimonapour <a.monapoor@gmail.com>
2022-09-25 01:42:50 +0330

    implement filtered music video with genres and release year

--- src/components/MusicVideos.js
+++ src/components/MusicVideos.js
@@ -1,10 +1,11 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import clsx from 'clsx'
 import useLazyLoad from './utils/useLazyLoad'
 import { Card } from './UI/Card'
 import PageSpinner from './UI/PageSpinner'
 import { LoadingMusicVideos } from './LoadingMusicVideos'
 import { useFetchData } from './hooks/useFetchData'
+import Select from 'react-select'
 
 const NUM_PER_PAGE = 20
 const TOTAL_PAGES = 250
@@ -14,11 +15,18 @@ function sortByNewestToOld(first, second) {
 }
 
 const MusicVideos = () => {
-  const { allMusicVideos, isLoading, isError } = useFetchData()
+  const [musicVideoGenreOptions, setMusicVideoGenreOptions] = useState()
+  const [musicVideoReleaseYear, setMusicVideoReleaseYear] = useState()
+  const { allMusicVideos, isLoading, isError, allGenres } = useFetchData()
+  const musicVideosReleaseYear = [
+    ...new Map(
+      allMusicVideos?.map(({ release_year }) => [release_year, release_year]),
+    ).values(),
+  ]
 
   const triggerRef = useRef(null)
   const onGrabData = (currentPage) => {
-    const musicVideos = allMusicVideos
+    let musicVideos = allMusicVideos
       .sort(sortByNewestToOld)
       .slice(
         ((currentPage - 1) % TOTAL_PAGES) * NUM_PER_PAGE,
@@ -26,7 +34,52 @@ const MusicVideos = () => {
       )
     return musicVideos
   }
-  const { data: musicVideos, loading } = useLazyLoad({ triggerRef, onGrabData })
+  let { data: musicVideos, loading } = useLazyLoad({ triggerRef, onGrabData })
+
+  function filterVideosByYear(musicVideoReleaseYear) {
+    if (musicVideoReleaseYear?.length > 0) {
+      musicVideos = allMusicVideos.filter((data) =>
+        musicVideoReleaseYear.find((i) => i.value === data.release_year),
+      )
+    }
+
+    return musicVideos
+  }
+
+  filterVideosByYear(musicVideoReleaseYear)
+
+  function filterVideosByGenre(musicVideoGenreOptions) {
+    if (musicVideoGenreOptions?.length > 0) {
+      musicVideos = allMusicVideos.filter((data) =>
+        musicVideoGenreOptions.find((i) => i.value === data.genre_id),
+      )
+    }
+    return musicVideos
+  }
+
+  filterVideosByGenre(musicVideoGenreOptions)
+
+  let genreListItems = []
+  for (let i = 0; i < allGenres?.length; i++) {
+    genreListItems.push({ value: allGenres[i].id, label: allGenres[i].name })
+  }
+
+  let yearListItems = []
+  for (let i = 0; i < musicVideosReleaseYear?.length; i++) {
+    yearListItems.push({
+      value: musicVideosReleaseYear[i],
+      label: musicVideosReleaseYear[i],
+    })
+  }
+
+  const handleGenreOptionChanges = (data) => {
+    setMusicVideoGenreOptions(data)
+  }
+
+  const handleYeraOptionChanges = (data) => {
+    setMusicVideoReleaseYear(data)
+  }
+
   return (
     <div>
       {isError ? (
@@ -40,7 +93,29 @@ const MusicVideos = () => {
         <PageSpinner />
       ) : (
         <>
-          <div className='grid grid-cols-16 gap-4'>
+          <div className='grid items-center gap-2.5 grid-cols-16'>
+            <div>
+              <Select
+                options={genreListItems}
+                placeholder='Select genre'
+                value={musicVideoGenreOptions}
+                onChange={handleGenreOptionChanges}
+                isSearchable={true}
+                isMulti
+              />
+            </div>
+            <div>
+              <Select
+                options={yearListItems}
+                placeholder='Select year'
+                value={musicVideoReleaseYear}
+                onChange={handleYeraOptionChanges}
+                isSearchable={true}
+                isMulti
+              />
+            </div>
+          </div>
+          <div className='grid grid-cols-16 gap-4 mt-3'>
             {musicVideos.map((video) => {
               return (
                 <Card
