5e0edce2b198c2bba92c798ccd9ae378feef637c
alimonapour <a.monapoor@gmail.com>
2022-09-24 19:17:38 +0330

    add styles to card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        16: 'repeat(auto-fit, minmax(240px, 1fr))',
+
+        // Complex site-specific column configuration
+        footer: '200px minmax(900px, 1fr) 100px',
+      },
+    },
     container: {
       center: true,
     },
