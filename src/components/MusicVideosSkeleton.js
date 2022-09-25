2ba76fbe28776f940d9fc9055ea9824156b9f98c
alimonapour <a.monapoor@gmail.com>
2022-09-25 16:31:42 +0330

    change loading music video to music videos skelton and changed som styles

---
+++ src/components/MusicVideosSkeleton.js
@@ -0,0 +1,24 @@
+export const CardSkeleton = () => {
+  return (
+    <div className='w-full rounded overflow-hidden shadow-lg'>
+      <div className='w-full h-64 bg-gray-300 animate-pulse'></div>
+      <div className='flex flex-col items-center px-6 py-4 '>
+        <div className=' mb-2 w-64 h-12 bg-gray-300 animate-pulse'>
+          <h1 className='w-full text-center text-base'>{}</h1>
+          <h2 className='text-sm tracking-tighter mt-1 text-center'>{}</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export const MusicVideosSkeleton = () => {
+  const skeletonRange = [...Array(10).keys()].map((n) => n + 1)
+  return (
+    <div className='grid sm:grid-cols-16 lg:grid-cols-4 gap-4 animate-pulse'>
+      {skeletonRange.map((num) => {
+        return <CardSkeleton key={num} />
+      })}
+    </div>
+  )
+}
