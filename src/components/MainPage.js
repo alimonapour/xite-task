8b37fb2ca02898b19f0b1aacec1c36a058b65205
alimonapour <a.monapoor@gmail.com>
2022-09-25 16:33:14 +0330

    add error to main page when occured error

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,33 +1,45 @@
 import React from 'react'
+import { useFetchData } from './hooks/useFetchMusicVideos'
 import MusicVideos from './MusicVideos'
 import DiscoverMusicVideos from './DiscoverMusicVideos'
 import ScrollButton from './UI/ScrollToTopButton'
 const MainPage = () => {
   const [query, setQuery] = React.useState('')
-
+  const { isError } = useFetchData()
   function handleINputChange(e) {
     e.preventDefault()
-
     setQuery(e.target.value)
   }
 
   return (
-    <div className='mt-5'>
-      <input
-        required
-        placeholder='Search music videos...'
-        className='serch-input'
-        value={query}
-        type='text'
-        onChange={handleINputChange}
-      />
-
-      {query?.length > 0 ? (
-        <DiscoverMusicVideos query={query} />
+    <div>
+      {isError ? (
+        <div className='flex flex-col items-center justify-between text-rose-600 text-lg mt-3'>
+          <p>There was an error:</p>
+          <>
+            <pre>An error occurred while fetching the data.</pre>
+            <pre>Please try again later.</pre>
+          </>
+        </div>
       ) : (
-        <MusicVideos />
+        <div className='mt-5'>
+          <input
+            required
+            placeholder='Search music videos...'
+            className='serch-input'
+            value={query}
+            type='text'
+            onChange={handleINputChange}
+          />
+
+          {query?.length > 0 ? (
+            <DiscoverMusicVideos query={query} />
+          ) : (
+            <MusicVideos />
+          )}
+          <ScrollButton />
+        </div>
       )}
-      <ScrollButton />
     </div>
   )
 }
