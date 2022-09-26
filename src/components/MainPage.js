fa12e617b29068b491dcf61c39260839380e849a
alimonapour <a.monapoor@gmail.com>
2022-09-26 21:41:17 +0330

    add filter options to main page

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,14 +1,79 @@
 import React from 'react'
 import { useFetchData } from './hooks/useFetchMusicVideos'
 import MusicVideos from './MusicVideos'
-import DiscoverMusicVideos from './DiscoverMusicVideos'
 import ScrollButton from './UI/ScrollToTopButton'
+import Select from 'react-select'
+
 const MainPage = () => {
   const [query, setQuery] = React.useState('')
-  const { isError } = useFetchData()
+  const [musicVideoGenreOptions, setMusicVideoGenreOptions] = React.useState([])
+  const [musicVideoReleaseYear, setMusicVideoReleaseYear] = React.useState([])
+  const { isError, allMusicVideos, allGenres, isLoading } = useFetchData()
+
+  const musicVideosReleaseYear = [
+    ...new Map(
+      allMusicVideos?.map(({ release_year }) => [release_year, release_year]),
+    ).values(),
+  ]
+
+  function filterMusicVideos(
+    musicVideoReleaseYear,
+    musicVideoGenreOptions,
+    query,
+  ) {
+    let filteredList = allMusicVideos
+    if (musicVideoGenreOptions?.length > 0) {
+      filteredList = filteredList.filter((musicVideo) =>
+        musicVideoGenreOptions.find((i) => i.value === musicVideo.genre_id),
+      )
+    }
+
+    if (musicVideoReleaseYear?.length > 0) {
+      filteredList = filteredList.filter((musicVideo) =>
+        musicVideoReleaseYear.find((i) => i.value === musicVideo.release_year),
+      )
+    }
+
+    if (query.length > 0) {
+      filteredList = filteredList.filter(({ artist, title }) => {
+        return Object.values({ artist, title }).some((val) =>
+          String(val).toLowerCase().includes(query),
+        )
+      })
+    }
+    return filteredList
+  }
+
+  const filteredMusicVideos = filterMusicVideos(
+    musicVideoReleaseYear,
+    musicVideoGenreOptions,
+    query,
+  )
+
   function handleINputChange(e) {
     e.preventDefault()
     setQuery(e.target.value)
+  }
+
+  let genreListItems = []
+  for (let i = 0; i < allGenres?.length; i++) {
+    genreListItems.push({ value: allGenres[i].id, label: allGenres[i].name })
+  }
+
+  let yearListItems = []
+  for (let i = 0; i < musicVideosReleaseYear?.length; i++) {
+    yearListItems.push({
+      value: musicVideosReleaseYear[i],
+      label: musicVideosReleaseYear[i],
+    })
+  }
+
+  const handleGenreOptionChanges = (data) => {
+    setMusicVideoGenreOptions(data)
+  }
+
+  const handleYeraOptionChanges = (data) => {
+    setMusicVideoReleaseYear(data)
   }
 
   return (
@@ -32,11 +97,36 @@ const MainPage = () => {
             onChange={handleINputChange}
           />
 
-          {query?.length > 0 ? (
-            <DiscoverMusicVideos query={query} />
-          ) : (
-            <MusicVideos />
-          )}
+          <div className='grid items-center gap-2.5 sm: grid-cols-16 lg:grid-cols-2'>
+            <div>
+              <Select
+                options={genreListItems}
+                placeholder='Select genre'
+                value={musicVideoGenreOptions}
+                onChange={handleGenreOptionChanges}
+                isSearchable={true}
+                isMulti
+              />
+            </div>
+            <div>
+              <Select
+                options={yearListItems}
+                placeholder='Select year'
+                value={musicVideoReleaseYear}
+                onChange={handleYeraOptionChanges}
+                isSearchable={true}
+                isMulti
+              />
+            </div>
+          </div>
+          <MusicVideos
+            musicVideos={filteredMusicVideos}
+            isLoading={isLoading}
+            dependencies={[
+              musicVideosReleaseYear.length,
+              musicVideoGenreOptions.length,
+            ]}
+          />
           <ScrollButton />
         </div>
       )}
