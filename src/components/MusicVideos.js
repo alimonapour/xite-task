d2394fc12f5b622fb8b84981f8f66a1ea6da805f
alimonapour <a.monapoor@gmail.com>
2022-09-26 21:42:13 +0330

    remove filter options from music videos component and move to main page

--- src/components/MusicVideos.js
+++ src/components/MusicVideos.js
@@ -1,10 +1,8 @@
-import { useRef, useState } from 'react'
+import { useRef } from 'react'
 import clsx from 'clsx'
-import useLazyLoad from './utils/useLazyLoad'
+import useLazyLoad from './hooks/useLazyLoad'
 import { Card } from './UI/Card'
 import { MusicVideosSkeleton } from './MusicVideosSkeleton'
-import { useFetchData } from './hooks/useFetchMusicVideos'
-import Select from 'react-select'
 
 const PAGE_SIZE = 20
 
@@ -12,122 +10,29 @@ function sortByNewestToOld(first, second) {
   return second.release_year - first.release_year
 }
 
-const MusicVideos = () => {
-  const [musicVideoGenreOptions, setMusicVideoGenreOptions] = useState()
-  const [musicVideoReleaseYear, setMusicVideoReleaseYear] = useState()
-  const { allMusicVideos, isLoading, allGenres } = useFetchData()
-  const musicVideosReleaseYear = [
-    ...new Map(
-      allMusicVideos?.map(({ release_year }) => [release_year, release_year]),
-    ).values(),
-  ]
-
+const MusicVideos = ({ musicVideos, isLoading }) => {
   const triggerRef = useRef(null)
+
   const onGrabData = (currentPage) => {
-    const totalPagesCount = allMusicVideos.length / PAGE_SIZE
-    let musicVideos = allMusicVideos
-      .sort(sortByNewestToOld)
+    const totalPagesCount = musicVideos?.length / PAGE_SIZE
+    const newList = musicVideos
+      ?.sort(sortByNewestToOld)
       .slice(
         ((currentPage - 1) % totalPagesCount) * PAGE_SIZE,
         PAGE_SIZE * (currentPage % totalPagesCount),
       )
-    return musicVideos
+    return newList
   }
-  let {
-    data: musicVideos,
-    loading,
-    currentPage,
-  } = useLazyLoad({
+
+  let { data, loading, currentPage } = useLazyLoad({
     triggerRef,
     onGrabData,
   })
 
-  let hasMore = currentPage * PAGE_SIZE <= allMusicVideos?.length
-  function filterVideosByYear(musicVideoReleaseYear) {
-    if (musicVideoReleaseYear?.length > 0) {
-      musicVideos = allMusicVideos.filter((data) =>
-        musicVideoReleaseYear.find((i) => i.value === data.release_year),
-      )
-    }
-
-    return musicVideos
-  }
-
-  const filteredMusicViodeosByYear =
-    musicVideoReleaseYear?.length >= 1
-      ? filterVideosByYear(musicVideoReleaseYear)
-      : []
-
-  hasMore =
-    filteredMusicViodeosByYear?.length >= 1
-      ? currentPage * PAGE_SIZE <= filteredMusicViodeosByYear?.length
-      : currentPage * PAGE_SIZE <= allMusicVideos?.length
-
-  function filterVideosByGenre(musicVideoGenreOptions) {
-    if (musicVideoGenreOptions?.length > 0) {
-      musicVideos = allMusicVideos.filter((data) =>
-        musicVideoGenreOptions.find((i) => i.value === data.genre_id),
-      )
-    }
-    return musicVideos
-  }
-
-  const filteredMusicViodeosByGenres =
-    musicVideoGenreOptions?.length >= 1
-      ? filterVideosByGenre(musicVideoGenreOptions)
-      : []
-
-  hasMore =
-    filteredMusicViodeosByGenres?.length >= 1
-      ? currentPage * PAGE_SIZE <= filteredMusicViodeosByGenres?.length
-      : currentPage * PAGE_SIZE <= allMusicVideos?.length
-
-  let genreListItems = []
-  for (let i = 0; i < allGenres?.length; i++) {
-    genreListItems.push({ value: allGenres[i].id, label: allGenres[i].name })
-  }
-
-  let yearListItems = []
-  for (let i = 0; i < musicVideosReleaseYear?.length; i++) {
-    yearListItems.push({
-      value: musicVideosReleaseYear[i],
-      label: musicVideosReleaseYear[i],
-    })
-  }
-
-  const handleGenreOptionChanges = (data) => {
-    setMusicVideoGenreOptions(data)
-  }
-
-  const handleYeraOptionChanges = (data) => {
-    setMusicVideoReleaseYear(data)
-  }
+  let hasMore = currentPage * PAGE_SIZE <= musicVideos?.length
 
   return (
     <div>
-      <div className='grid items-center gap-2.5 grid-cols-16'>
-        <div>
-          <Select
-            options={genreListItems}
-            placeholder='Select genre'
-            value={musicVideoGenreOptions}
-            onChange={handleGenreOptionChanges}
-            isSearchable={true}
-            isMulti
-          />
-        </div>
-        <div>
-          <Select
-            options={yearListItems}
-            placeholder='Select year'
-            value={musicVideoReleaseYear}
-            onChange={handleYeraOptionChanges}
-            isSearchable={true}
-            isMulti
-          />
-        </div>
-      </div>
-
       {isLoading ? (
         <div className='mt-3'>
           <MusicVideosSkeleton />
@@ -135,7 +40,7 @@ const MusicVideos = () => {
       ) : (
         <>
           <div className='grid sm:grid-cols-16 lg:grid-cols-4 gap-4 mt-3'>
-            {musicVideos.map((video) => {
+            {data?.map((video) => {
               return (
                 <Card
                   key={video.id}
