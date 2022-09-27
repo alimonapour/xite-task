700ecbafa90dff6b8c9f86d58966abe348230d45
alimonapour <a.monapoor@gmail.com>
2022-09-27 08:07:32 +0330

    create music video list component and replace with music videos component

---
+++ src/components/MusicVideosList.js
@@ -0,0 +1,37 @@
+import { useRef } from 'react'
+import { Card } from './UI/Card'
+import { useLazyLoad } from './hooks/useLazyLoad'
+
+export default function MusicVideosList({ items }) {
+  console.log(6, '>>>', items)
+  const loaderTriggerRef = useRef(null)
+  const { visibleItems, isLastPage } = useLazyLoad(
+    items,
+    loaderTriggerRef.current,
+  )
+
+  return (
+    <div className='grid grid-cols-16 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-3'>
+      {visibleItems?.length > 0 ? (
+        visibleItems.map((item) => (
+          <Card
+            key={item.id}
+            artist={item.artist}
+            title={item.title}
+            imageUrl={item.image_url}
+          />
+        ))
+      ) : (
+        <p className='text-base font-semibold text-center'>
+          Hmmm... There were no music videos found with this search.
+          <br /> Please try another.
+        </p>
+      )}
+      <div
+        ref={loaderTriggerRef}
+        style={{ height: '100px' }}
+        hidden={isLastPage}
+      ></div>
+    </div>
+  )
+}
