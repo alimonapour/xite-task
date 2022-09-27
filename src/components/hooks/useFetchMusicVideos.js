22d49d1179d35449b4d9d08d2b9b726d42fadf05
alimonapour <a.monapoor@gmail.com>
2022-09-27 08:09:00 +0330

    add sort by year function for sorting music videos

--- src/components/hooks/useFetchMusicVideos.js
+++ src/components/hooks/useFetchMusicVideos.js
@@ -3,11 +3,14 @@ import useSWR from 'swr'
 const apiUrl =
   'https://raw.githubusercontent.com/XiteTV/frontend-coding-exercise/main/data/dataset.json'
 
+function sortByNewestToOld(first, second) {
+  return second.release_year - first.release_year
+}
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 export function useFetchData() {
   const { data, error } = useSWR(apiUrl, fetcher)
   return {
-    allMusicVideos: data?.videos,
+    allMusicVideos: data?.videos.sort(sortByNewestToOld),
     allGenres: data?.genres,
     isLoading: !error && !data,
     isError: error,
