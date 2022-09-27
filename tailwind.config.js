e2dc1cb20f8d1fde76e2060c2054483c4af51a3a
alimonapour <a.monapoor@gmail.com>
2022-09-27 08:01:52 +0330

    add font to search input place holder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -5,12 +7,21 @@ module.exports = {
       gridTemplateColumns: {
         // Simple 16 column grid
         16: 'repeat(auto-fit, minmax(240px, 1fr))',
-
-        // Complex site-specific column configuration
-        footer: '200px minmax(900px, 1fr) 100px',
       },
-      backgroundImage: {
-        'hero-pattern': "url('components/assets/xite.png')",
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Oxygen',
+          'Ubuntu',
+          'Cantarell',
+          'Fira Sans',
+          'Droid Sans',
+          'Helvetica Neue',
+          ...fontFamily.sans,
+        ],
       },
     },
     container: {
